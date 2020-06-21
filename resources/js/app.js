2dba439701bb8f095f9191363b9c602ecc5c2966
Igor <iip2205@mail.ru>
2020-06-21 17:21:49 +0400

    Set blade and vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
-import Main from './Main.vue';
+import App from './App.vue';
+import router from './router';
 import 'bootstrap/dist/css/bootstrap.css'
 
 new Vue({
     el: '#app',
-    render: h => h(Main)
+    router,
+    render: h => h(App)
 });
